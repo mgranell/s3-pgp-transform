d75973523a1124b9e04bd9aede94da51fe255378
Martin Granell <martin@memewave.com>
2020-03-09 14:16:48 +1100

    Test fix

--- test/dectest.sh
+++ test/dectest.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 if [ -f dectest.md ];then rm dectest.md;fi
+if [ -f dectest.md.pgp ];then rm dectest.md.pgp;fi
 
 gpg --batch --trust-model always -r pgp@test.local -o dectest.md.pgp -e ../README.md
 aws s3 cp dectest.md.pgp s3://mg-s3-pgp-dec-bucket/
